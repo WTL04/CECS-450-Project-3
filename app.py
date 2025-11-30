d8867729479e6b97a183b112b93f6f73e0dacaf0
Brizeydas <hernandez.brizeyda@yahoo.com>
2025-11-30 15:31:05 -0800

    Change app.run_server to app.run

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def update_all(selected_aoi):
     return cockpit_figure(selected_aoi), build_bar_figure(selected_aoi), build_main_figure(selected_aoi)
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
